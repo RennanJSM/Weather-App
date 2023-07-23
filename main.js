d9a7d10a698eeb340efd9a9864299bebd54a909c
RennanJSM <87147821+RennanJSM@users.noreply.github.com>
2023-07-23 15:21:47 -0300

    weather app

---
+++ main.js
@@ -0,0 +1,18 @@
+const request = require("request");
+const dotenv = require("dotenv").config();
+
+const argument = process.argv[2];
+const apikey = process.env.APIKEY;
+
+const options = {
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${argument}&appid=${apikey}&units=metric`,
+    method: "GET",
+    json: true,
+}
+
+
+request(options, (error, res, body) => {
+    //console.log(body.main.temp);
+    console.log(`A temperatura atual em ${argument} é de ${body.main.temp} °C`)
+
+});
